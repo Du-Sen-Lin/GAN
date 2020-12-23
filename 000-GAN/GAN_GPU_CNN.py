ead9f554659bd69c858dcebcde9c76a843ada6ff
ldusl0713 <dusl0713@thundersoft.com>
2020-12-23 22:50:14 +0800

    add 000-GAN

---
+++ 000-GAN/GAN_GPU_CNN.py
@@ -0,0 +1,191 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision import datasets
+from torchvision.utils import save_image
+import os
+
+if not os.path.exists('./img'):
+    os.mkdir('./img')
+
+
+def to_img(x):
+    out = 0.5 * (x + 1)
+    out = out.clamp(0, 1)
+    out = out.view(-1, 1, 28, 28)
+    return out
+
+
+batch_size = 128
+num_epoch = 10
+z_dimension = 100   #noise dimension
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(0.5, 0.5)
+])
+
+mnist = datasets.MNIST(root='../data/mnist/',
+                       train=True, transform=img_transform, download=True)
+
+dataloader = DataLoader(mnist, batch_size=batch_size, shuffle=True, num_workers=4)
+
+"""Conv2d
+二维卷积常用于计算机视觉、图像处理领域。
+输入输出数据格式
+input ： （batch_size , channel , height , width）
+out : （batch_size , channel , height , width）
+"""
+
+
+class discriminator(nn.Module):
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 5, padding=2),  # batch, 32, 28, 28
+            nn.LeakyReLU(0.2, True),
+            nn.AvgPool2d(2, stride=2),  # batch, 32, 14, 14
+            )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 5, padding=2),  # batch, 64, 14, 14
+            nn.LeakyReLU(0.2, True),
+            nn.AvgPool2d(2, stride=2)  # batch, 64, 7, 7
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(64*7*7, 1024),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        '''
+        x: batch, width, height, channel=1
+        '''
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+"""
+nn.BatchNorm2d(num_features):
+    1.对小批量(mini-batch)3d数据组成的4d输入进行批标准化(Batch Normalization)操作
+    2.num_features： 
+            来自期望输入的特征数，该期望输入的大小为'batch_size x num_features x height x width'
+            （输入输出相同）
+                输入Shape：（N, C，H, W)
+                输出Shape：（N, C, H, W）
+      eps： 为保证数值稳定性（分母不能趋近或取0）,给分母加上的值。默认为1e-5。
+      momentum： 动态均值和动态方差所使用的动量。默认为0.1。
+      affine： 一个布尔值，当设为true，给该层添加可学习的仿射变换参数。
+    3.在每一个小批量（mini-batch）数据中，计算输入各个维度的均值和标准差。gamma与beta是可学习的大小为C的参数向量（C为输入大小）
+      在训练时，该层计算每次输入的均值与方差，并进行移动平均。移动平均默认的动量值为0.1。
+      在验证时，训练求得的均值/方差将用于标准化验证数据。
+    4.例子
+        >>> # With Learnable Parameters
+        >>> m = nn.BatchNorm2d(100) #num_features指的是randn(20, 100, 35, 45)中（N, C，H, W)的第二维C
+        >>> # Without Learnable Parameters
+        >>> m = nn.BatchNorm2d(100, affine=False)
+        >>> input = autograd.Variable(torch.randn(20, 100, 35, 45))  #输入Shape：（N, C，H, W)
+        >>> output = m(input)
+"""
+
+
+class generator(nn.Module):
+    def __init__(self, input_size, num_feature):
+        super(generator, self).__init__()
+        self.fc = nn.Linear(input_size, num_feature)  # batch, 3136=1x56x56
+        self.br = nn.Sequential(
+            nn.BatchNorm2d(1),
+            nn.ReLU(True)
+        )
+        self.downsample1 = nn.Sequential(
+            nn.Conv2d(1, 50, 3, stride=1, padding=1),  # batch, 50, 56, 56
+            nn.BatchNorm2d(50),
+            nn.ReLU(True)
+        )
+        self.downsample2 = nn.Sequential(
+            nn.Conv2d(50, 25, 3, stride=1, padding=1),  # batch, 25, 56, 56
+            nn.BatchNorm2d(25),
+            nn.ReLU(True)
+        )
+        self.downsample3 = nn.Sequential(
+            nn.Conv2d(25, 1, 2, stride=2),  # batch, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(x.size(0), 1, 56, 56)
+        x = self.br(x)
+        x = self.downsample1(x)
+        x = self.downsample2(x)
+        x = self.downsample3(x)
+        return x
+
+
+D = discriminator().cuda() #discriminator model
+G = generator(z_dimension, 3136).cuda() #genetator model
+
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0003)
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
+
+# train
+for epoch in range(num_epoch):
+    for i, (img, _) in enumerate(dataloader):
+        num_img = img.size(0)
+        # ========train discriminator
+        real_img = Variable(img).cuda()
+        real_label = Variable(torch.ones(num_img)).cuda()
+        fake_label = Variable(torch.zeros(num_img)).cuda()
+
+        # compute loss of real_img
+        real_out = D(real_img)
+        d_loss_real = criterion(real_out, real_label)
+        real_scores = real_out  # closer to 1 means better
+
+        # compute loss of fake_img
+        z = Variable(torch.randn(num_img, z_dimension)).cuda()
+        fake_img = G(z)
+        fake_out = D(fake_img)
+        d_loss_fake = criterion(fake_out, fake_label)
+        fake_scores = fake_out  # closer to 0 means better
+
+        # bp and optimize
+        d_loss = d_loss_real + d_loss_fake
+        d_optimizer.zero_grad()
+        d_loss.backward()
+        d_optimizer.step()
+
+        # ============train generator
+        # compute loss of fake_img
+        z = Variable(torch.randn(num_img, z_dimension)).cuda()
+        fake_img = G(z)
+        output = D(fake_img)
+        g_loss = criterion(output, real_label)
+
+        # bp and optimize
+        g_optimizer.zero_grad()
+        g_loss.backward()
+        g_optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print('Epoch[{}/{}], d_loss: {:.6f}, g_loss:{:.6f}'
+                  'D real: {:.6f}, D fake: {:.6f}'.format(
+                epoch, num_epoch, d_loss.item(), g_loss.item(),
+                real_scores.data.mean(), fake_scores.data.mean()
+            ))
+    if epoch == 0:
+        real_images = to_img(real_img.cpu().data)
+        save_image(real_images, './img/real_images.png')
+
+    fake_images = to_img(fake_img.cpu().data)
+    save_image(fake_images, './img/fake_images-{}.png'.format(epoch + 1))
+
+torch.save(G.state_dict(), './models/generator_CNN.pth')
+torch.save(D.state_dict(), './models/discriminator_CNN.pth')
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # GAN
+
+Generative Adversarial network（生成对抗网络)
+
+概述：GAN（生成对抗网络），顾名思义，这个网络第一部分是生成网络，第二部分对抗模型严格来讲是一个判别器；简单来说，就是让两个网络相互竞争，生成网络来生成假的数据，对抗网络通过判别器去判别真伪，最后希望生成器生成的数据能够以假乱真。
+
+核心思想：判断器的任务是尽力将假的判断为假的，将真的判断为真的；生成器的任务是使生成的越真越好。两者交替迭代训练。
+
+Discriminator Network(对抗网络)
+
+Generative Network（生成网络）
+
+环境：
+
+CUDA10.1 ; Pytorch1.5.1 ; python 3.7 ......
+
+## 0、前置
+
+理解GAN: 博客
+
+1、简单的多层神经网络（Multilayer neural network）
+
+训练速度快：10轮：CPU——14mins, GPU——6mins ；50轮之后才有效果
+
+2、简单的卷积神经网络（Convolutional Neural Network）
+
+GPU, 10轮左右效果就能展现出来了。
+
+## 1、Auxiliary Classifier GAN
+
+
+
+
+
+
+
+
+
